bff95130eb6aa5800206f921456b4a4a8064ba44
yuxintao <1921056015@qq.com>
2023-08-17 15:24:19 +0800

    创建SSM项目，使用配置文件形式

---
+++ src/main/java/org/jingtao8a/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.jingtao8a.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
