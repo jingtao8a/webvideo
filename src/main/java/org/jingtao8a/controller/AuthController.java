cf8c1861aef4d9c12992d036c588e303fbc4700b
yuxintao <1921056015@qq.com>
2023-08-21 14:29:48 +0800

    更换为接受json数据体

--- src/main/java/org/jingtao8a/controller/AuthController.java
+++ src/main/java/org/jingtao8a/controller/AuthController.java
@@ -5,41 +5,37 @@ import org.jingtao8a.model.User;
 import org.jingtao8a.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @Controller
+@RequestMapping("/auth")
 public class AuthController {
     @Autowired
     UserService userService;
-    @RequestMapping(value="/register", method= RequestMethod.POST)
+    @RequestMapping(value="register", method= RequestMethod.POST)
     @ResponseBody
-    public Result register(@RequestParam String userName, @RequestParam String email, @RequestParam String password, @RequestParam String img) {
-        List<User> userList = userService.getUsersByUserName(userName);
+    public Result register(@RequestBody User user) {
+        List<User> userList = userService.getUsersByUserName(user.getUserName());
         if (userList.size() != 0) {
             return new Result(409, "userName has been used");
         }
-        userList = userService.getUsersByEmail(email);
+        userList = userService.getUsersByEmail(user.getEmail());
         if (userList.size() != 0) {
             //email 已经被使用
             return new Result(409, "email has been used");
         }
-        System.out.println(userName);
-        System.out.println(email);
-        userService.addUser(new User(null, userName, email, password, img));
+        userService.addUser(user);
         return new Result(200, "register success");
     }
 
-    @RequestMapping(value="/login", method= RequestMethod.POST)
+    @RequestMapping(value="login", method= RequestMethod.POST)
     public void login() {
 
     }
 
-    @RequestMapping(value="/logout", method= RequestMethod.POST)
+    @RequestMapping(value="logout", method= RequestMethod.POST)
     public void logout() {
 
     }
